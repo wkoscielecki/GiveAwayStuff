0145508b2ed4aa808c4d314b8d57ee0eb2146bd1
wkoscielecki <wojciech11@poczta.onet.pl>
2019-05-08 18:48:25 +0200

    first commit

---
+++ src/main/java/com/koscielecki/app/GiveAwayStuffApplication.java
@@ -0,0 +1,13 @@
+package com.koscielecki.app;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GiveAwayStuffApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(GiveAwayStuffApplication.class, args);
+    }
+
+}
